a6008084207f206d278caaaaff88a8c65d3fa954
JINXZ <jinxz@jinxz.dev>
2024-01-03 02:58:27 -0500

    add support for urls

--- koalahook.py
+++ koalahook.py
@@ -115,8 +115,13 @@ def changepfp(url):
         image_path = input(f"{yellow}[? KOALAHOOK ?]{white} Path to the image file: ")
     
     try:
-        with open(image_path, "rb") as image_file:
-            encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
+        if image_path.startswith(('http://', 'https://')):
+            response = requests.get(image_path)
+            response.raise_for_status()
+            encoded_image = base64.b64encode(response.content).decode('utf-8')
+        else:
+            with open(image_path, "rb") as image_file:
+                encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
 
         data = {
             "avatar": f"data:image/jpeg;base64,{encoded_image}"
